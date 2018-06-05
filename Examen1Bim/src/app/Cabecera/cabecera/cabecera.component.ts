fedd20ab4aeae1157dacad989375538ed69524a1
MaritzaRodriguez <maritza.rodriguez.enc@gmail.com>
2018-06-05 13:32:04 -0500

    pantallas, componentes, boton limpiar

--- Examen1Bim/src/app/Cabecera/cabecera/cabecera.component.ts
+++ Examen1Bim/src/app/Cabecera/cabecera/cabecera.component.ts
@@ -8,20 +8,32 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./cabecera.component.css']
 })
 export class CabeceraComponent implements OnInit {
-  @Input() nombre: string;
-  @Input() versionApi: number;
-  @Input() fechaLanzamiento: date;
-  @Input() pesoGB: float;
-  @Input() instalado: boolean;
-  constructor() { }
+  // @Input() nombre: string;
+  // @Input() versionApi: number;
+  // @Input() fechaLanzamiento: date;
+  // @Input() pesoGB: float;
+  // @Input() instalado: boolean;
+
+  nombre = '';
+  versionApi = '';
+  fechalanzamiento = '';
+  pesoGB = '';
+  instalado = '';
+
+  constructor() {
+  }
+
 
   ngOnInit() {
 
-
-
-
-
-
   }
 
+  funcionlimpiar() {
+    this.nombre = '  ';
+    this.fechalanzamiento = '  ';
+    this.versionApi = '  ';
+    this.pesoGB = '  ';
+    this.instalado = '  ';
+
+  }
 }
